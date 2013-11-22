bcbfa49306bbb80c9b36cab62dd7d482c0d5e8ac
Dirk Thomas <dthomas@osrfoundation.org>
2013-11-22 10:30:19 -0800

    rqt_image_view: wrap cv calls in try-catch-block (fix #201)

--- rqt_image_view/src/rqt_image_view/image_view.cpp
+++ rqt_image_view/src/rqt_image_view/image_view.cpp
@@ -253,35 +253,44 @@ void ImageView::callbackImage(const sensor_msgs::Image::ConstPtr& msg)
   }
   catch (cv_bridge::Exception& e)
   {
-    // If we're here, there is no conversion that makes sense, but let's try to imagine a few first
-    cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg);
-    if (msg->encoding == "CV_8UC3")
+    try
     {
-      // assuming it is rgb
-      conversion_mat_ = cv_ptr->image;
-    } else if (msg->encoding == "8UC1") {
-      // convert gray to rgb
-      cv::cvtColor(cv_ptr->image, conversion_mat_, CV_GRAY2RGB);
-    } else if (msg->encoding == "16UC1" || msg->encoding == "32FC1") {
-      // scale / quantify
-      double min = 0;
-      double max = ui_.max_range_double_spin_box->value();
-      if (msg->encoding == "16UC1") max *= 1000;
-      if (ui_.dynamic_range_check_box->isChecked())
+      // If we're here, there is no conversion that makes sense, but let's try to imagine a few first
+      cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg);
+      if (msg->encoding == "CV_8UC3")
       {
-        // dynamically adjust range based on min/max in image
-        cv::minMaxLoc(cv_ptr->image, &min, &max);
-        if (min == max) {
-          // completely homogeneous images are displayed in gray
-          min = 0;
-          max = 2;
+        // assuming it is rgb
+        conversion_mat_ = cv_ptr->image;
+      } else if (msg->encoding == "8UC1") {
+        // convert gray to rgb
+        cv::cvtColor(cv_ptr->image, conversion_mat_, CV_GRAY2RGB);
+      } else if (msg->encoding == "16UC1" || msg->encoding == "32FC1") {
+        // scale / quantify
+        double min = 0;
+        double max = ui_.max_range_double_spin_box->value();
+        if (msg->encoding == "16UC1") max *= 1000;
+        if (ui_.dynamic_range_check_box->isChecked())
+        {
+          // dynamically adjust range based on min/max in image
+          cv::minMaxLoc(cv_ptr->image, &min, &max);
+          if (min == max) {
+            // completely homogeneous images are displayed in gray
+            min = 0;
+            max = 2;
+          }
         }
+        cv::Mat img_scaled_8u;
+        cv::Mat(cv_ptr->image-min).convertTo(img_scaled_8u, CV_8UC1, 255. / (max - min));
+        cv::cvtColor(img_scaled_8u, conversion_mat_, CV_GRAY2RGB);
+      } else {
+        qWarning("ImageView.callback_image() could not convert image from '%s' to 'rgb8' (%s)", msg->encoding.c_str(), e.what());
+        ui_.image_frame->setImage(QImage());
+        return;
       }
-      cv::Mat img_scaled_8u;
-      cv::Mat(cv_ptr->image-min).convertTo(img_scaled_8u, CV_8UC1, 255. / (max - min));
-      cv::cvtColor(img_scaled_8u, conversion_mat_, CV_GRAY2RGB);
-    } else {
-      qWarning("ImageView.callback_image() could not convert image from '%s' to 'rgb8' (%s)", msg->encoding.c_str(), e.what());
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      qWarning("ImageView.callback_image() while trying to convert image from '%s' to 'rgb8' an exception was thrown (%s)", msg->encoding.c_str(), e.what());
       ui_.image_frame->setImage(QImage());
       return;
     }
