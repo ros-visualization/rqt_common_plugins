b3cb644eeedbc446ee911b48fb3704bfe7c67790
ojura <juraj5@gmail.com>
2016-05-04 08:23:45 +0200

    remove reference to Qt5

--- rqt_reconfigure/src/rqt_reconfigure/paramedit_widget.py
+++ rqt_reconfigure/src/rqt_reconfigure/paramedit_widget.py
@@ -111,7 +111,7 @@ class ParameditWidget(QWidget):
         # Add color to alternate the rim of the widget.
         LayoutUtil.alternate_color(self._dynreconf_clients.itervalues(),
             [self.palette().window().color().lighter(125),
-            self.palette().window().color().darker(125)]) # Qt5
+            self.palette().window().color().darker(125)])
 
     def close(self):
         for dc in self._dynreconf_clients:
