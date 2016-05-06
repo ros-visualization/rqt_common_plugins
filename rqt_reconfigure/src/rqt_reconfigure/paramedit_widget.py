8161d7dcab33590204678dfd6d10f84811266bcc
ojura <juraj5@gmail.com>
2016-05-06 19:29:14 +0200

    rqt_reconfigure: fix accessing attribute superseded in Qt5 (#370)
    * rqt_reconfigure: fix accessing attribute superseded in Qt5
    
    * use only window() attribute
    
    * remove reference to Qt5
    
    * fix indentation to conform to PEP8

--- rqt_reconfigure/src/rqt_reconfigure/paramedit_widget.py
+++ rqt_reconfigure/src/rqt_reconfigure/paramedit_widget.py
@@ -109,9 +109,10 @@ class ParameditWidget(QWidget):
             #    self.vlayout.addWidget(v)
 
         # Add color to alternate the rim of the widget.
-        LayoutUtil.alternate_color(self._dynreconf_clients.itervalues(),
-                                   [self.palette().background().color().lighter(125),
-                                    self.palette().background().color().darker(125)])
+        LayoutUtil.alternate_color(
+            self._dynreconf_clients.itervalues(),
+            [self.palette().window().color().lighter(125),
+             self.palette().window().color().darker(125)])
 
     def close(self):
         for dc in self._dynreconf_clients:
